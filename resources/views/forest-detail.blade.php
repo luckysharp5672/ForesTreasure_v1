88424c38b0118cc6a42660fad5ef40567210c8c9
luckysharp5672 <luckysharp5672@gmail.com>
2023-09-17 15:38:04 +0000

    テーブルにoverfloを適用

--- resources/views/forest-detail.blade.php
+++ resources/views/forest-detail.blade.php
@@ -75,11 +75,11 @@
         <div class="flex-1 text-gray-700 text-left px-4 py-2 m-2">
              <!-- 立木の一覧 -->
             <div class="flex flex-col">
-              <div class="-m-1.5 overflow-x-auto">
+              <div class="-m-1.5 overflow-x-auto" style="height: 600px; overflow-y: auto;">
                 <div class="p-1.5 min-w-full inline-block align-middle">
                   <div class="border rounded-lg shadow overflow-hidden dark:border-gray-700 dark:shadow-gray-900">
                     <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                      <thead class="bg-gray-50 dark:bg-gray-700">
+                      <thead class="bg-gray-50 dark:bg-gray-700 sticky top-0">
                         <tr>
                           <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400">森林ID</th>
                           <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase dark:text-gray-400">立木ID</th>
